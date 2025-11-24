1178423f40c4d52393387686c3cb53f3c5c73c8c
Taras Kovalyk <tarik2454@gmail.com>
2025-11-24 18:50:09 +0200

    Refactor project structure to use React with Vite; update index.html, remove unused files, and enhance README with setup instructions.

---
+++ src/data/card-data.ts
@@ -0,0 +1,38 @@
+export const cardData = [
+  {
+    id: "card1",
+    wrapperClass: "card1",
+    frontSrc: "https://deckofcardsapi.com/static/img/JH.png",
+    alt: "Jack of Hearts",
+  },
+  {
+    id: "card2",
+    wrapperClass: "card2",
+    frontSrc: "https://deckofcardsapi.com/static/img/QH.png",
+    alt: "Queen of Hearts",
+  },
+  {
+    id: "card3",
+    wrapperClass: "card3",
+    frontSrc: "https://deckofcardsapi.com/static/img/KH.png",
+    alt: "King of Hearts",
+  },
+  {
+    id: "card4",
+    wrapperClass: "card4",
+    frontSrc: "https://deckofcardsapi.com/static/img/AH.png",
+    alt: "Ace of Hearts",
+  },
+  {
+    id: "card5",
+    wrapperClass: "card5",
+    frontSrc: "https://deckofcardsapi.com/static/img/0H.png",
+    alt: "Ten of Hearts",
+  },
+  {
+    id: "card6",
+    wrapperClass: "card6",
+    frontSrc: "https://deckofcardsapi.com/static/img/9H.png",
+    alt: "Nine of Hearts",
+  },
+];
